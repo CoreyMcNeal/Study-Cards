9c79c6ede586a4e789e2f5fe6fb069c8b86b6293
Corey McNeal <mcnealcorey@icloud.com>
2021-09-03 16:46:23 -0700

    -Added more to the skeleton of the GUI with buildIntro()

--- src/UserInterface/StudyGUI.java
+++ src/UserInterface/StudyGUI.java
@@ -1,9 +1,55 @@
 package UserInterface;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class StudyGUI {
     
-    
+    private JFrame frame;
+
+    private JPanel introPanel;
+    private JLabel introQuestionLabel;
+    private JTextField introQuestionEntry;
+    private JLabel introAnswerLabel;
+    private JTextField introAnswerEntry;
+
+    private JButton introAddButton;
+    private JButton introSubmitButton;
+
     public void start() {
+
+    }
+
+    public void buildPanelsAndFrame() {
+        frame = new JFrame();
+
+
+        
+
+        frame.add(introPanel, BorderLayout.CENTER);
+        frame.setTitle("Study Cards");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+    }
+
+    private void buildIntro() {
+
+        introPanel = new JPanel();
+        introPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        introPanel.setLayout(new GridBagLayout());
+
+        // Add components to panel***********
 
     }
 }
