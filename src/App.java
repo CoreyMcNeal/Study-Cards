211b39cdebb06c53e188d5b8b39a1c321be474d1
Corey McNeal <mcnealcorey@icloud.com>
2021-09-03 19:48:52 -0700

    Finished the visual GUI for the intro panel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import UserInterface.StudyGUI;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        StudyGUI gui = new StudyGUI();
+        gui.start();
     }
 }
--- src/UserInterface/StudyGUI.java
+++ src/UserInterface/StudyGUI.java
@@ -1,7 +1,11 @@
 package UserInterface;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -9,34 +13,38 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-public class StudyGUI {
+public class StudyGUI implements ActionListener{
     
     private JFrame frame;
 
     private JPanel introPanel;
+    private GridBagConstraints introConstraints;
+    private JLabel introWelcomeLabel;
     private JLabel introQuestionLabel;
     private JTextField introQuestionEntry;
     private JLabel introAnswerLabel;
     private JTextField introAnswerEntry;
-
     private JButton introAddButton;
-    private JButton introSubmitButton;
+    private JButton introFinishedButton;
+
 
     public void start() {
-
+        buildPanelsAndFrame();
     }
 
     public void buildPanelsAndFrame() {
         frame = new JFrame();
 
 
-        
+        buildIntro();
 
         frame.add(introPanel, BorderLayout.CENTER);
         frame.setTitle("Study Cards");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 400);
+        frame.setSize(500, 500);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
@@ -48,8 +56,71 @@ public class StudyGUI {
         introPanel = new JPanel();
         introPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         introPanel.setLayout(new GridBagLayout());
+        introConstraints = new GridBagConstraints();
 
         // Add components to panel***********
+        introWelcomeLabel = new JLabel("Welcome to Study Cards, Java Edition!", SwingConstants.CENTER);
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 0;
+        introPanel.add(introWelcomeLabel, introConstraints);
 
+        introQuestionLabel = new JLabel("Enter Question: ");
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 1;
+        introQuestionLabel.setBorder(new EmptyBorder(20, 0, 10, 0));
+        introPanel.add(introQuestionLabel, introConstraints);
+
+        introQuestionEntry = new JTextField();
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 2;
+        introQuestionEntry.setPreferredSize(new Dimension(200, 20));
+        introPanel.add(introQuestionEntry, introConstraints);
+
+        introAnswerLabel = new JLabel("Enter Answer: ");
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 3;
+        introAnswerLabel.setBorder(new EmptyBorder(15, 0, 10, 0));
+        introPanel.add(introAnswerLabel, introConstraints);
+
+        introAnswerEntry = new JTextField();
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 4;
+        introAnswerEntry.setPreferredSize(new Dimension(200, 20));
+        introPanel.add(introAnswerEntry, introConstraints);
+
+        JLabel spacer = new JLabel("");
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 5;
+        spacer.setPreferredSize(new Dimension(0, 15));
+        introPanel.add(spacer, introConstraints);
+
+        introAddButton = new JButton("Add Card");
+        introAddButton.addActionListener(this);
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 6;
+        introAddButton.setPreferredSize(new Dimension(200, 100));
+        introPanel.add(introAddButton, introConstraints);
+
+        JLabel spacer2 = new JLabel("");
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 7;
+        spacer.setPreferredSize(new Dimension(0, 15));
+        introPanel.add(spacer2, introConstraints);
+
+        introFinishedButton = new JButton("Start Studying");
+        introFinishedButton.addActionListener(this);
+        introFinishedButton.setEnabled(false);
+        introConstraints.gridx = 0;
+        introConstraints.gridy = 8;
+        introFinishedButton.setPreferredSize(new Dimension(200, 100));
+        introPanel.add(introFinishedButton, introConstraints);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == introAddButton) {
+            introFinishedButton.setEnabled(true);
+        }
+        
     }
 }
